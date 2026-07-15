4212ac72f6b9e36f6630f51e5b18b831c5498e04
mirdukkkkk <karmulav@gmail.com>
2026-07-16 03:41:10 +1200

    molten is initialized ⚡

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+    description = "Development environment for Pixel Battle Molten";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs {
+                    inherit system;
+                };
+            in
+            {
+                devShells.default = pkgs.mkShell {
+                    packages = with pkgs; [
+                        go_1_26
+                        ffmpeg
+                        protobuf
+                        protoc-gen-go
+                    ];
+                };
+            }
+        );
+}
